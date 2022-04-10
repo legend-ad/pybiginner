28c0d9a9c8bc9258c7e82a8745c8a36660d203a1
legend-ad <61944537+legend-ad@users.noreply.github.com>
2022-04-10 13:10:51 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,11 @@
+# take input and print
+name=input("Enter your name : ")
+print(name)
+#input is stored as string
+
+
+#incase of using number for addition 
+a=int(input("Enter the number A = "))
+b=int(input("Enter the number B = "))
+c=a+b
+print("Total = ",c)
