5926d0c6b0d0452915990ee16958fa34b20d447c
Arjun v Nath <61944537+legend-ad@users.noreply.github.com>
2022-04-11 20:39:52 +0530

    Add files via upload

---
+++ strfunc.py
@@ -0,0 +1,8 @@
+s=input('ENTER THE NAME ')
+print(s)
+print(s.find('a')) #find a
+print(s.count('a')) #count a
+print(s.capitalize()) #capitalising
+print(s.upper()) #uppercase
+print(s.format())
+print(s.lower())
