d9b1a4acbc8abc11af51992a95cfccaa9da96c58
Arjun v Nath <61944537+legend-ad@users.noreply.github.com>
2022-04-13 12:35:43 +0530

    Add files via upload

---
+++ arithamatic.py
@@ -0,0 +1,14 @@
+#Arithamatic operation
+
+from __future__ import division
+
+
+a=5
+b=2
+print(a+b)      #addition
+print(a-b)      #sustraction
+print(a*b)      #multiplication
+print(a/b)      #division
+print(a//b)     #floordivision
+print(a**b)     #exponention
+print(a%b)      #modulus
