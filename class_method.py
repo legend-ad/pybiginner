6ac7e9c47f657bff3c786b32f3a16e22886f4d40
Arjun v Nath <61944537+legend-ad@users.noreply.github.com>
2022-04-27 14:18:56 +0530

    Add files via upload

---
+++ class_method.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+
+
+class student:
+    name= 'Arjun'
+    age=20
+    def printall():
+         print('Name : ',student.name)
+         print('Age : ',student.age)
+student.printall()
